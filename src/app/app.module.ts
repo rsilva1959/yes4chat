b2879a52b5d96aaa2b2c0db436dd065f3fd7dd1d
pferreiro <pedroferreiro95@gmail.com>
2019-02-08 17:03:57 +0000

    group-chat

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { ChatComponent } from './chat/chat.component';
+import { HomeComponent } from './home/home.component';
+import { environment } from '../environments/environment';
+
+
+@NgModule({
+  declarations: [AppComponent, ChatComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD9HZ7pVtzRBqkiB_RPjNckJuqqBnwDMT0",
+    authDomain: "firechat-39975.firebaseapp.com",
+    databaseURL: "https://firechat-39975.firebaseio.com",
+    projectId: "firechat-39975",
+    storageBucket: "firechat-39975.appspot.com",
+    messagingSenderId: "506917814444"
+  }
+};
